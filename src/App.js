b14ab5e55ac71dcabb9ce5228a8aa780bc529901
Abin Simon <abinsimon10@gmail.com>
2017-11-17 20:01:18 +0530

    Set up auth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import firebase from './firebase.js';
+
+import User from './components/User/User'
+
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      threads: [],
+      user: null,
+    }
+
+    this.handleAuth = this.handleAuth.bind(this)
+    this.addThread = this.addThread.bind(this)
+  }
+
+  handleAuth(user){
+    let curState = this.state
+    curState.user = user
+    this.setState(curState)
+  }
+
+  addThread(){
+    console.log('this.state.user: ', this.state.user);
+    if (this.state.user) {
+      const content =
+        {
+          title: "Smooth :/",
+          content: "Wooo, chill there kid",
+          ceatedAt: Date.now(),
+          posts: [],
+          user: {
+            uid: this.state.user.uid,
+            displyName: this.state.user.displayName,
+            email: this.state.user.email,
+            displayImage: this.state.user.photoURL
+          }
+        }
+      firebase.database().ref('threads').push(content);
+    }
+  }
+
+  jsonToList(data){
+    let list = []
+    for( var i in data ){
+      list.push({ "id": i, "data": data[i]})
+    }
+    return list
+  }
+
+  componentDidMount(){
+    let threadsRef = firebase.database().ref('threads')
+    threadsRef.on('value', snapshot => {
+      let curState = this.state
+      curState.threads = this.jsonToList(snapshot.val())
+      this.setState(curState)
+    })
+  }
+
+
   render() {
+    console.log('this.state.threads: ', this.state.threads);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <User handle={this.handleAuth} />
+
+        <div className="threads">
+          { this.state.threads.map( thread => {
+            return (
+              <p key={thread.id}>{ thread.data.title }</p>
+            )
+          })
+          }
       </div>
+
+      <button onClick={this.addThread}> Add </button>
+    </div>
     );
   }
 }
---
+++ src/components/User/User.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import './User.css'
+
+import firebase, { auth, provider } from '../../firebase.js';
+
+class User extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: null
+    }
+
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  addUserToDB(user){
+    let userRef = firebase.database().ref('users/' + user.uid)
+    let userInfo = {
+      "displayName": user.displayName,
+      "displayImage": user.photoURL,
+      "lastLogin": Date.now(),
+      "email": user.email,
+      "provider": "google.com",
+      "registeredAt": Date.now(),
+      "likes": [],
+      "threads": [],
+      "posts": [],
+      "admin": false,  // Can be only changed from firebase console
+    }
+    userRef.once('value', snap => {
+      console.log('snap.val(): ', snap.val());
+      const d = snap.val()
+      // firebase does not create space for empty values
+      if (d.threads)
+        userInfo.threads = d.threads
+      if (d.likes)
+        userInfo.likes = d.likes
+      if (d.posts)
+        userInfo.posts = d.posts
+      if (d.registeredAt)
+        userInfo.registeredAt = d.registeredAt
+      firebase.database().ref('users/' + user.uid).set({ ...userInfo });
+    })
+  }
+
+  login() {
+    auth.signInWithPopup(provider)
+      .then((result) => {
+        const user = result.user;
+        this.setState({
+          user
+        });
+        this.addUserToDB(user)
+        this.props.handle(this.state.user)
+      });
+  }
+
+  logout() {
+    auth.signOut()
+      .then(() => {
+        this.setState({
+          user: null
+        });
+        this.props.handle(this.state.user)
+      });
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+        this.addUserToDB(user)
+        this.props.handle(this.state.user)
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="User">
+        {this.state.user ?
+          <div>
+            <p>{ this.state.user.displayName }</p>
+            <button onClick={this.logout}>Log Out</button>
+          </div>
+        :
+          <button onClick={this.login}>Log In</button>
+        }
+        </div>
+        );
+  }
+
+}
+
+export default User
