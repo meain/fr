1cae6fb2f229355379b92de78580003440ec465c
Abin Simon <abinsimon10@gmail.com>
2017-11-23 09:31:45 +0530

    add newpost button

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { connect } from 'react-redux'
 import './App.css';
 
 import { updateThreads, userChanged } from './reducers.js'
+import firebase from './firebase.js'
 
 import 'antd/dist/antd.css';
 import { BackTop } from 'antd'
 
 import User from './components/User/User';
+import Editor from './components/Editor/Editor'
 import ThreadList from './components/ThreadList/ThreadList';
 import ThreadDetails from './components/ThreadDetails/ThreadDetails';
 
@@ -17,11 +19,37 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.handleAuth = this.handleAuth.bind(this);
+    this.handleAuth = this.handleAuth.bind(this)
+    this.addThread = this.addThread.bind(this)
   }
 
   handleAuth(user) {
     this.props.userChanged(user)
+  }
+
+  addThread(data) {
+    if (this.props.user) {
+      const thread =
+        {
+          title: data.title,
+          content: data.content,
+          createdAt: Date.now(),
+          posts: [],  // Sub posts, we will most probably have only one ( jsut in case )
+          likes: [],  // Will contain user uids
+          stars: [],  // Bookmark question ( implement later )
+          user: {
+            uid: this.props.user.uid,
+            displayName: this.props.user.displayName,
+            email: this.props.user.email,
+            displayImage: this.props.user.photoURL
+          }
+        };
+      const key = firebase.database().ref('threads').push(thread).key;
+      console.log('key: ', key);
+      // Add theard data to user
+      firebase.database().ref('users/' + this.props.user.uid + '/threads/' + key).set(true);
+    }
+    // Display what they just posted or route back to main page
   }
 
 
@@ -35,12 +63,11 @@ class App extends Component {
 
           <br/>
 
-          {/* <Editor onSubmit={this.addThread} /> */}
-
           <br/>
           <div>
 
             <Route exact path="/" render={({history}) => (<ThreadList history={history}/>) } />
+              <Route path="/newPost" render={ () => (<Editor onSubmit={this.addThread} />) } />
           <Route path="/thread/:threadId" render={ () => (<ThreadDetails />) }/>
 
           </div>
--- src/components/ThreadList/ThreadList.js
+++ src/components/ThreadList/ThreadList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
 import './ThreadList.css';
 
@@ -14,7 +16,7 @@ class ThreadList extends Component {
   constructor(props){
     super(props)
 
-    this.addThread = this.addThread.bind(this);
+    // this.addThread = this.addThread.bind(this);
   }
 
   componentDidMount(){
@@ -40,29 +42,29 @@ class ThreadList extends Component {
     }
   }
 
-  addThread(data) {
-    if (this.props.user) {
-      const thread =
-        {
-          title: data.title,
-          content: data.content,
-          createdAt: Date.now(),
-          posts: [],  // Sub posts, we will most probably have only one ( jsut in case )
-          likes: [],  // Will contain user uids
-          stars: [],  // Bookmark question ( implement later )
-          user: {
-            uid: this.props.user.uid,
-            displayName: this.props.user.displayName,
-            email: this.props.user.email,
-            displayImage: this.props.user.photoURL
-          }
-        };
-      const key = firebase.database().ref('threads').push(thread).key;
-      console.log('key: ', key);
-      // Add theard data to user
-      firebase.database().ref('users/' + this.props.user.uid + '/threads/' + key).set(true);
-    }
-  }
+  // addThread(data) {
+  //   if (this.props.user) {
+  //     const thread =
+  //       {
+  //         title: data.title,
+  //         content: data.content,
+  //         createdAt: Date.now(),
+  //         posts: [],  // Sub posts, we will most probably have only one ( jsut in case )
+  //         likes: [],  // Will contain user uids
+  //         stars: [],  // Bookmark question ( implement later )
+  //         user: {
+  //           uid: this.props.user.uid,
+  //           displayName: this.props.user.displayName,
+  //           email: this.props.user.email,
+  //           displayImage: this.props.user.photoURL
+  //         }
+  //       };
+  //     const key = firebase.database().ref('threads').push(thread).key;
+  //     console.log('key: ', key);
+  //     // Add theard data to user
+  //     firebase.database().ref('users/' + this.props.user.uid + '/threads/' + key).set(true);
+  //   }
+  // }
 
   handleClick(ev, key){
     this.props.history.push('/thread/' + key)
@@ -71,7 +73,9 @@ class ThreadList extends Component {
   render(){
     return (
       <div className="threads">
-        { this.props.user &&  <Editor onSubmit={this.addThread} />}
+
+          { this.props.user && <Link to="/newPost" ><button> New Post</button> </Link>}
+        {/* { this.props.user &&  <Editor onSubmit={this.addThread} />} */}
         { this.props.threads.map( thread => {
           return (
             <Thread
