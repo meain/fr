15245790590612ba76d9b6b318efba303d67d4a9
Abin Simon <abinsimon10@gmail.com>
2017-11-24 20:01:43 +0530

    fix scroll on thread/

--- src/components/ThreadDetails/ThreadDetails.js
+++ src/components/ThreadDetails/ThreadDetails.js
@@ -19,7 +19,25 @@ class ThreadDetails extends Component {
     }
 
     this.addPost = this.addPost.bind(this)
+    this.scrollToTop()
   }
+
+  scrollToTop(scrollDuration) {
+    const scrollHeight = window.scrollY,
+      scrollStep = Math.PI / (scrollDuration / 15),
+      cosParameter = scrollHeight / 2;
+    var scrollCount = 0,
+      scrollMargin,
+      scrollInterval = setInterval(function () {
+        if (window.scrollY != 0) {
+          scrollCount = scrollCount + 1;
+          scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
+          window.scrollTo(0, (scrollHeight - scrollMargin));
+        }
+        else clearInterval(scrollInterval);
+      }, 15);
+  }
+
 
   componentDidMount() {
     let posts = this.props.thread.posts
