4a0edcdc436d41be9aed8568672e70975ac68a7e
Abin Simon <abinsimon10@gmail.com>
2017-11-25 10:03:30 +0530

    fix loading for threadlist

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Icon from 'react-fa'
+import loadingMessages from './LoadingMessages'
+
+import './Loading.css'
+
+class Loading extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            loadingMessage: "Loading thread list. Just hang on.",
+            loadingMessages: loadingMessages
+        }
+    }
+
+    componentDidMount(){
+        let messages = this.state.loadingMessages
+        setInterval(() => {
+            this.setState({
+                ...this.state,
+                loadingMessage: messages[Math.floor(Math.random() * messages.length)]
+            })
+        }, 5000);
+    }
+
+    render() {
+        return (
+            <div className="Loading">
+                <Icon spin name="adjust" className="Loading-spinner" />
+                <p>
+                <span className="muted monospace">LOADING</span>
+                </p>
+                <p>
+                <span className="muted monospace">{this.state.loadingMessage}</span>
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Loading
--- src/components/ThreadList/ThreadList.js
+++ src/components/ThreadList/ThreadList.js
@@ -6,6 +6,7 @@ import fuzzysearch from 'fuzzysearch'
 import './ThreadList.css';
 import firebase from '../../firebase.js';
 import { updateThreads, userChanged } from '../../reducers.js'
+import Loading from '../Loading/Loading'
 
 import Thread from '../Thread/Thread'
 
@@ -15,16 +16,21 @@ class ThreadList extends Component {
     super(props)
 
     this.state = {
-      search: ""
+      search: "",
+      loading: true
     }
 
     this.searchChange = this.searchChange.bind(this)
   }
 
   componentDidMount() {
+    if (this.props.threads && this.props.threads.length > 0) {
+      this.setState({ ...this.state, loading: false })
+    }
     let threadsRef = firebase.database().ref('threads')
     threadsRef.on('value', snapshot => {
       this.props.threadsChanged(snapshot.val())
+      this.setState({ ...this.state, loading: false })
     })
   }
 
@@ -33,7 +39,7 @@ class ThreadList extends Component {
   }
 
   searchChange(e) {
-    this.setState({ search: e.target.value })
+    this.setState({ ...this.state, search: e.target.value })
   }
 
   getDisplayThreads() {
@@ -51,54 +57,60 @@ class ThreadList extends Component {
     let threads = this.getDisplayThreads()
     return (
       <div className="ThreadList">
-
-        <form className="form">
-          <div className="form-item">
-            <input type="text" className="search ThreadList-search"
-              onChange={this.searchChange} placeholder="Search" />
-            {this.props.user && threads.length !== 0 &&
-              <div className="ThreadList-search-ask">
-                <kbd> Search for existing question or{' '}
-                  <Link to="/newPost" >
-                    ask a new question
+        {
+          this.state.loading ?
+            <Loading />
+            :
+            <div>
+              <form className="form">
+                <div className="form-item">
+                  <input type="text" className="search ThreadList-search"
+                    onChange={this.searchChange} placeholder="Search" />
+                  {this.props.user && threads.length !== 0 &&
+                    <div className="ThreadList-search-ask">
+                      <kbd> Search for existing question or{' '}
+                        <Link to="/newPost" >
+                          ask a new question
           </Link>
-                  .
+                        .
           </kbd>
-              </div>
-            }
-        { !this.props.user && threads.length !== 0  &&
-              <div className="ThreadList-search-ask">
-            <kbd>Login to ask a question or just feel free to check out existing ones.</kbd>
-              </div>
-        }
-          </div>
-        </form>
-        { !this.props.user && threads.length === 0  &&
-          <div className="ThreadList-ask">
-            <kbd> Looks like we don't have what you are looking for. Login to ask a question.</kbd>
-          </div>
-        }
-        {this.props.user && threads.length === 0 &&
-          <div className="ThreadList-ask">
-            <kbd> Looks like we don't have what you are looking for, would you like to{' '}
-              <Link to="/newPost" >
-                ask a question
+                    </div>
+                  }
+                  {!this.props.user && threads.length !== 0 &&
+                    <div className="ThreadList-search-ask">
+                      <kbd>Login to ask a question or just feel free to check out existing ones.</kbd>
+                    </div>
+                  }
+                </div>
+              </form>
+              {!this.props.user && threads.length === 0 &&
+                <div className="ThreadList-ask">
+                  <kbd> Looks like we don't have what you are looking for. Login to ask a question.</kbd>
+                </div>
+              }
+              {this.props.user && threads.length === 0 &&
+                <div className="ThreadList-ask">
+                  <kbd> Looks like we don't have what you are looking for, would you like to{' '}
+                    <Link to="/newPost" >
+                      ask a question
           </Link>
-              ?
+                    ?
           </kbd>
-          </div>
-        }
+                </div>
+              }
 
-        {threads.map(thread =>
-          <Thread
-            key={thread.id}
-            user={this.props.user}
-            postKey={thread.id}
-            data={thread.data}
-            bordered={true}
-            handleClick={(e) => this.handleClick(e, thread.id)}
-          />
-        )}
+              {threads.map(thread =>
+                <Thread
+                  key={thread.id}
+                  user={this.props.user}
+                  postKey={thread.id}
+                  data={thread.data}
+                  bordered={true}
+                  handleClick={(e) => this.handleClick(e, thread.id)}
+                />
+              )}
+            </div>
+        }
       </div>
     )
   }
