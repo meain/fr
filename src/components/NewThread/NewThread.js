27ab132e2c8a9d6201a83b4fff40773b9a4fb409
Abin Simon <abinsimon10@gmail.com>
2017-11-30 12:30:47 +0530

    Make sure user is logged in before asking a question

--- src/components/NewThread/NewThread.js
+++ src/components/NewThread/NewThread.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Icon from 'react-fa'
 
 import firebase from '../../firebase.js'
 import { userChanged } from '../../reducers.js'
@@ -8,7 +9,7 @@ import './NewThread.css'
 
 import Editor from '../Editor/Editor'
 
-class NewThead extends Component{
+class NewThead extends Component {
   constructor(props) {
     super(props)
 
@@ -17,38 +18,57 @@ class NewThead extends Component{
 
   addThread(data) {
     if (this.props.user) {
-      const thread =
-        {
-          title: data.title,
-          content: data.content,
-          createdAt: Date.now(),
-          posts: [],  // Sub posts, we will most probably have only one ( jsut in case )
-          likes: [],  // Will contain user uids
-          stars: [],  // Bookmark question ( implement later )
-          user: this.props.user.uid,
-        }
-      const key = firebase.database().ref('threads').push(thread).key
+      const thread = {
+        title: data.title,
+        content: data.content,
+        createdAt: Date.now(),
+        posts: [], // Sub posts, we will most probably have only one ( jsut in case )
+        likes: [], // Will contain user uids
+        stars: [], // Bookmark question ( implement later )
+        user: this.props.user.uid
+      }
+      const key = firebase
+        .database()
+        .ref('threads')
+        .push(thread).key
       // Add theard data to user
-      firebase.database().ref('users/' + this.props.user.uid + '/threads/' + key).set(true)
+      firebase
+        .database()
+        .ref('users/' + this.props.user.uid + '/threads/' + key)
+        .set(true)
       this.props.history.push('/')
     }
   }
 
-  render(){
-    return(
+  handleAuth(user) {
+    this.props.userChanged(user)
+  }
+
+  render() {
+    return (
       <div className="NewThread">
-        <br/>
-        <h2>New Question</h2>
-        <Editor hasHeading={true} onSubmit={this.addThread} />
+        {this.props.user ? (
+          <div>
+            <br />
+            <h2>New Question</h2>
+            <Editor hasHeading={true} onSubmit={this.addThread} />
+          </div>
+        ) : (
+          <div class="NewThread-not-signed-in">
+            <Icon name="user-o" className="NewThread-user-icon" />
+            <p>
+              <var>Login to ask a new question</var>
+            </p>
+          </div>
+        )}
       </div>
     )
   }
-
 }
 
 const mapStateToProps = state => {
   return {
-    user: state.user,
+    user: state.user
   }
 }
 
